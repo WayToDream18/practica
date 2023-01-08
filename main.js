605ffab3c3333849340d4f54ba503deb828fcaf8
WayToDream18 <114391247+WayToDream18@users.noreply.github.com>
2023-01-08 16:55:58 +0300

    finish

---
+++ main.js
@@ -0,0 +1,19 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+
+const personalMovieDb = {
+	count:numberOfFilms,
+	movies:{},
+	actors:{},
+	genres:[],
+	privat:false
+};
+
+let a = prompt('Один из просмотренных фильмов?', '');
+b = prompt('На сколько оцените его?','');
+c = prompt('Один из просмотренных фильмов?','');
+d = prompt('На сколько оцените его?','');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+console.log(personalMovieDb);
